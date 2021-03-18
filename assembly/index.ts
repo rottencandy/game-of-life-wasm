5aa6c726ed801c5feee67ae189613c5cd9fb1bda
Mohammed Saud <md.saud020@gmail.com>
2021-03-18 13:40:54 +0530

    implement life
    "let there be life"

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,120 @@
 // The entry file of your WebAssembly module.
+memory.grow(1);
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+let universe_width: u32;
+let universe_height: u32;
+let alive_color: u32;
+let dead_color: u32;
+let chunk_offset: u32;
+
+@inline
+function getCell(x: u32, y: u32): u32 {
+  return load<u32>((x + y * universe_width) << 2);
+}
+
+@inline
+function setCell(x: u32, y: u32, val: u32): void {
+  store<u32>(((x + y * universe_width) << 2) + chunk_offset, val);
+}
+
+@inline
+function copyToPrimary(): void {
+  memory.copy(0, chunk_offset, chunk_offset);
+}
+
+function fillUniverse(): void {
+  for (let x: u32 = 0; x < universe_width; x++) {
+    for (let y: u32 = 0; y < universe_height; y++) {
+      setCell(x, y, Math.random() > 0.5 ? alive_color : dead_color);
+    }
+  }
+
+  copyToPrimary();
+}
+
+export function init(width: u32, height: u32): void {
+  universe_width = width;
+  universe_height = height;
+
+  alive_color = 0xffc39800;
+  dead_color = 0xfff4f4f4;
+  chunk_offset = width * height * 4;
+
+  fillUniverse();
+}
+
+function countNeighbours(x: u32, y: u32): u32 {
+  let neighbours = 0;
+
+  const max_x = universe_width - 1;
+  const max_y = universe_height - 1;
+
+  const y_above = y == 0 ? max_y : y - 1;
+  const y_below = y == max_y ? 0 : y + 1;
+  const x_left = x == 0 ? max_x : x - 1;
+  const x_right = x == max_x ? 0 : x + 1;
+
+  // top left
+  if(getCell(x_left, y_above) == alive_color) {
+    neighbours++;
+  }
+
+  // top
+  if(getCell(x, y_above) == alive_color) {
+    neighbours++;
+  }
+
+  // top right
+  if(getCell(x_right, y_above) == alive_color) {
+    neighbours++;
+  }
+
+  // right
+  if(getCell(x_right, y) == alive_color) {
+    neighbours++;
+  }
+
+  // bottom right
+  if(getCell(x_right, y_below) == alive_color) {
+    neighbours++;
+  }
+
+  // bottom
+  if(getCell(x, y_below) == alive_color) {
+    neighbours++;
+  }
+
+  // bottom left
+  if(getCell(x_left, y_below) == alive_color) {
+    neighbours++;
+  }
+
+  // left
+  if(getCell(x_left, y) == alive_color) {
+    neighbours++;
+  }
+
+  return neighbours;
+}
+
+export function update(): void {
+  for (let x: u32 = 0; x < universe_width; x++) {
+    for (let y: u32 = 0; y < universe_height; y++) {
+
+      const neighbours = countNeighbours(x, y);
+
+      if (neighbours < 2) {
+        // less than 2 neighbours, cell is no longer alive
+        setCell(x, y, dead_color);
+      } else if (neighbours == 3) {
+        // cell will be alive
+        setCell(x, y, alive_color);
+      } else if (neighbours > 3) {
+        // cell dies due to overpopulation
+        setCell(x, y, dead_color);
+      }
+
+    }
+  }
+  copyToPrimary();
 }
--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-const imports = { /* imports go here */ };
-const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
-module.exports = wasmModule.exports;
+const WIDTH = 128, HEIGHT = 128, FPS = 10;
+
+const runWasm = async () => {
+  const importObject = {
+    env: {
+      seed: Date.now,
+      abort: () => console.log('aborting!')
+    }
+  };
+  const module = await WebAssembly.instantiateStreaming(fetch('./build/optimized.wasm'), importObject);
+  const exports = module.instance.exports;
+
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  exports.init(WIDTH, HEIGHT);
+
+  const memoryBuffer = exports.memory.buffer;
+  const memoryArray = new Uint8ClampedArray(memoryBuffer)
+  
+  const imageData = ctx.createImageData(WIDTH, HEIGHT);
+
+  const step = () => {
+    exports.update();
+  
+    imageData.data.set(memoryArray.slice(0, WIDTH * HEIGHT * 4));
+    ctx.putImageData(imageData, 0, 0);
+  
+    setTimeout(step, 1000 / FPS);
+  };
+  step();
+
+}
+runWasm();
